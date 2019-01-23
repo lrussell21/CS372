944d3d8a9ca424ccad35be815ffe21c2f2f39934
lrussell21 <lrussell21@my.whitworth.edu>
2019-01-22 18:15:04 -0800

    Upload

--- HW4/WebSpider/src/WebSpider.java
+++ HW4/WebSpider/src/WebSpider.java
@@ -1,78 +1,114 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
 import java.io.*;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.*;
 
 public class WebSpider {
-    ArrayList<String> links = new ArrayList<>();
-    ArrayList<String> emails = new ArrayList<>();
+    HashMap<String,Boolean> link = new HashMap<>();
+    Set<String> email = new HashSet<>();
 
     public WebSpider(){
-        String startAddress = "https://www.foxnews.com/";
-        links.add(startAddress);
-        boolean run = true;
+        // My high school's website has a lot of mailto emails...
+        String startAddress = "https://meadhs.mead354.org/ourpages/administration/";
+        link.put(startAddress, false);
 
-
+        // Loops through hashmap until 100 links have been followed.
         int j = 0;
-        while(run){
+        while(true){
             if(j > 100){
-                run = false;
+                break;
             }else{
-                search(links.get(j));
+                for(Map.Entry<String,Boolean> entry: link.entrySet()){
+                    if(!entry.getValue()){
+                        //System.out.println(entry.getKey());
+                        search(entry.getKey());
+                        break;
+                    }
+                }
+                System.out.println(j);
                 j++;
             }
         }
 
-        //listLinks();
-        //listEmails();
-
+        // Lists emails at end.
+        listEmails();
     }
 
     public void search(String address){
 
         try {
+            // I tried a ton of regex for link but none of them matched...
+            String regexURL = "<a\\s*?href=\"(http:.*?)\"";
+            String regexEmail = "mailto:(.*?)";
+
             URL url = new URL(address);
             BufferedReader rdr = new BufferedReader(new InputStreamReader(url.openStream()));
             String line;
-            int i=50;
-            while ((line = rdr.readLine()) != null && i > 0) {
+            int i = 1000;
+            line = rdr.readLine();
+            while (line != null && i > 0) {
 
-
-                // Get link if there is one.
-                String line2 = line;
-                Pattern email = Pattern.compile("\"mailto:(.*?)\"");
-                Matcher matcher = email.matcher(line2);
-                if (matcher.find()) {
-                    //System.out.printf("email: %s\n", matcher.group(0));
-                    //emails.add(matcher.group(1));
+                // Gets links
+                String[] linkparts = line.split("href=");
+                for(String p: linkparts) {
+                    for(int j = 1; j < p.length(); j++){
+                        // Makes sure http is at beginning and its not .pdf at end.
+                        if (p.charAt(j) == 34 && p.charAt(1) == 'h' && p.charAt(4) == 'p' && p.charAt(j - 1) != 'f') {
+                            link.put(p.substring(1, j),false);
+                            //System.out.printf("Added website: %s\n", p.substring(1, j));
+                            break;
+                        }
+                    }
                 }
 
-                Pattern webLink = Pattern.compile("<a\\s*?href=\"(http:.*?)\"\n");
+                // Gets emails
+                String[] emailparts = line.split("mailto");
+                boolean run = true;
+                for(String p: emailparts) {
+                    for(int j = 1; j < p.length(); j++){
+                        // Checks for " and makes sure its at a mailto:
+                        if(p.charAt(0) == ':' && p.charAt(j) == 34){
+                            email.add(p.substring(1, j));
+                            //System.out.printf("Added email: %s\n", p.substring(1, j));
+                            run = false;
+                            break;
+                        }
+                    }
+                    if(!run){
+                        break;
+                    }
+                }
+
+                // I tried to get this to work for hours but could never get it to work properly.
+                /*
+                Pattern webLink = Pattern.compile(regexURL, Pattern.CASE_INSENSITIVE);
                 Matcher webmatcher = webLink.matcher(line);
-                if (webmatcher.find()) {
+                if (webmatcher.matches()) {
+                    System.out.printf("TEST WEBSITE: %s\n", webmatcher.matches());
+                    //System.out.printf("GOT WEBSITE: %s\n", webmatcher.group(0));
+                    //System.out.printf("GOT WEBSITE: %s\n", webmatcher.group(1));
+                }
+                while (webmatcher.find()) {
                     //System.out.printf("Website: %s\n", webmatcher.group(0));
                     //links.add(webmatcher.group(1));
                 }
 
-                /*
-                String[] linkparts = line.split("href=");
-                for(String p: linkparts) {
-                    for(int j = 1; j < p.length(); j++){
-                        if(p.charAt(j) == 34){
-                            if (p.charAt(1) == 'h' && p.charAt(4) == 'p') {
-                                links.add(p.substring(1, j));
-                                System.out.printf("Added website: %s\n", p.substring(1, j));
-                                break;
-                            }
-                        }
-                    }
 
+                Pattern email = Pattern.compile(regexEmail);
+                Matcher matcher = email.matcher(line);
+                //System.out.printf("EMAIL TEST: %s\n", matcher.matches());
+                if (matcher.matches()) {
+                    System.out.printf("TEST EMAIL: %s\n", matcher.matches());
                 }
-*/
+                while (matcher.find()) {
+                    // System.out.printf("email: %s\n", matcher.group(1));
+                    //emails.add(matcher.group(1));
+                }
+                */
+                line = rdr.readLine();
                 i--;
             }
+            System.out.println("DONE");
         }
         catch (Exception ex) {
             System.out.printf("Oops: %s", ex.getMessage());
@@ -81,21 +117,13 @@ public class WebSpider {
 
     }
 
-    public void listLinks(){
-        for(String p: links){
-            System.out.println(p);
-        }
-    }
-
     public void listEmails(){
-        System.out.printf("\n\n\n\n\n OUTPUTING EMAILS:\n");
-        for(String p: emails){
-            System.out.println(p);
-        }
+        System.out.printf("\nOUTPUTING EMAILS:\n");
+        System.out.println(email);
     }
 
     public static void main(String[] args) {
-        WebSpider test = new WebSpider();
+        WebSpider spiderMan = new WebSpider();
     }
 
 }
